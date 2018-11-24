93f2da5f63c8e356f5f7c7dffe2519cb53abfee0
Kelvin Chow <kelvin.chow@gmail.com>
2018-11-24 03:31:02 -0600

    Clean up simplefft.py
    - Remove unused dependency on matplotlib
    - Save debug numpy images
    - Add readout oversampling cropping
    - Add ISMRM meta attribute data

--- simplefft.py
+++ simplefft.py
@@ -5,7 +5,6 @@ import itertools
 import logging
 import numpy as np
 import numpy.fft as fft
-import matplotlib.pyplot as plt
 
 
 def groups(iterable, predicate):
@@ -23,11 +22,6 @@ def process(connection, config, params):
     logging.info("Config: \n%s", config.decode("utf-8"))
     logging.info("Params: \n%s", params.decode("utf-8"))
 
-    # First group in the simple_gre.h5 dataset inexplicably contains 129 acquisitions.
-    # We discard one to preserve the sanity of the gadgetron ismrmrd client (it expects
-    # images that are 256x128, not 256x129).
-    discard = next(connection)
-
     for group in groups(connection, lambda acq: acq.is_flag_set(ismrmrd.ACQ_LAST_IN_SLICE)):
         image = process_group(group, config, params)
 
@@ -36,21 +30,52 @@ def process(connection, config, params):
 
 
 def process_group(group, config, params):
-
+    # Format data into single [cha RO PE] array
     data = [acquisition.data for acquisition in group]
-
-    logging.info("Processing %d acquisitions.", len(data))
-
     data = np.stack(data, axis=-1)
+
+    logging.info("Raw data is size %s" % (data.shape,))
+    np.save("/tmp/dependency/raw.npy", data)
+
+    # Fourier Transform
     data = fft.fftshift(data, axes=(1, 2))
     data = fft.ifft2(data)
     data = fft.ifftshift(data, axes=(1, 2))
-    data = np.abs(data)
 
+    # Sum of squares coil combination
+    data = np.abs(data)
     data = np.square(data)
     data = np.sum(data, axis=0)
     data = np.sqrt(data)
 
-    return ismrmrd.Image.from_array(data, acquisition=group[0])
+    logging.info("Image data is size %s" % (data.shape,))
+    np.save("/tmp/dependency/img.npy", data)
+
+    # Normalize and convert to int16
+    data *= 32768/data.max()
+    data = np.around(data)
+    data = data.astype(np.int16)
+
+    # Remove phase oversampling
+    nRO = np.size(data,0);
+    data = data[int(nRO/4):int(nRO*3/4),:]
+    logging.info("Image without oversampling is size %s" % (data.shape,))
+    np.save("/tmp/dependency/img_crop.npy", data)
+    
+    # Format as ISMRMRD image data
+    image = ismrmrd.Image.from_array(data, acquisition=group[0])
+    image.image_index = 1
+
+    # Set ISMRMRD Meta Attributes
+    meta = ismrmrd.Meta({'GADGETRON_DataRole': 'Image',
+                         'GADGETRON_ImageProcessingHistory': ['FIRE', 'PYTHON'],
+                         'GADGETRON_WindowCenter': '16384',
+                         'GADGETRON_WindowWidth': '32768'})
+    xml = meta.serialize()
+    logging.info("XML: %s", xml)
+    logging.info("Image data has %d elements", image.data.size)
+
+    image.attribute_string = xml
+    return image
 
 
