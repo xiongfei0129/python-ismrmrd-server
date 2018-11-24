7ca4c43e3d22c995447ddc28def6deea0d40e69a
Kelvin Chow <kelvin.chow@gmail.com>
2018-11-24 03:28:26 -0600

    Add ability to log to file

--- main.py
+++ main.py
@@ -20,18 +20,24 @@ def main(args):
 
 if __name__ == '__main__':
 
-    logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.WARNING)
-
     parser = argparse.ArgumentParser(description='I should be able to write this.',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument('-p', '--port', type=int, help='Port')
     parser.add_argument('-H', '--host', type=str, help='Host')
     parser.add_argument('-v', '--verbose', action='store_true', help='Verbose output.')
+    parser.add_argument('-l', '--logfile', type=str, help='Path to log file')
 
     parser.set_defaults(**defaults)
 
     args = parser.parse_args()
+
+    if args.logfile:
+        print("Logging to file: ", args.logfile)
+        logging.basicConfig(filename=args.logfile, format='%(asctime)s - %(message)s', level=logging.WARNING)
+    else:
+        print("No logfile provided")
+        logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.WARNING)
 
     if args.verbose:
         logging.root.setLevel(logging.DEBUG)
